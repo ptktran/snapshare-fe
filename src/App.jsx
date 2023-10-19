3cd4c58eecc8a9d2cd6abd9bc8ce29a21b8a19c3
Peter Tran <petertranminh22@gmail.com>
2023-10-18 20:59:17 -0400

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-500 text-xl">Snapshare!!!</h1>
+    </>
+  )
+}
+
+export default App
