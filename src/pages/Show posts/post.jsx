60cc5c8d3f46ee012392db298cbf10355b35eb75
sommaosakwe <102556642+sommaosakwe@users.noreply.github.com>
2023-10-26 22:48:00 -0400

    commit

---
+++ src/pages/Show posts/post.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Post({ post }) {
+  return (
+    <div className="post">
+      {post.type.startsWith('image') ? (
+        <img src={post.image_url} alt="Post" />
+      ) : (
+        <video controls>
+          <source src={post.image_url} type={post.type} />
+          Your browser does not support the video tag.
+        </video>
+      )}
+      <p>{post.caption}</p>
+    </div>
+  );
+}
+//added prop type validation using PropTypes.
+Post.propTypes = {
+  post: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
+export default Post;
