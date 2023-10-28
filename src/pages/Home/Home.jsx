c2c7022622ec146e6cb667da4aaf8124cad73763
sommaosakwe <102556642+sommaosakwe@users.noreply.github.com>
2023-10-28 10:31:38 -0400

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,35 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import Post from "./Post"; // Import the Post component
 
 export default function Home() {
-  const [posts, setPosts] = useState(null)
-  const [error, setError] = useState(null)
+  const [posts, setPosts] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:3000/getAllPosts`)
       .then((response) => {
         if (!response.ok) {
-          setError(response.statusText)
+          setError(response.statusText);
         }
-        return response.json()
+        return response.json();
       })
       .then((data) => setPosts(data.data))
       .catch((err) => {
-        setError(err)
-      })
-  })
+        setError(err);
+      });
+  }, []);
+
   return (
     <>
       <div className="h-max w-full flex justify-center items-center flex-col gap-5">
-        {posts && 
+        {posts &&
           posts.map((content, key) => (
-            <div key={key} className="border flex flex-col">
-              <img src={content.image_url}/>
-              <h1>{content.caption}</h1>
-              <h1>{content.created_at}</h1>
-            </div>
-        ))}
+            <Post key={key} post={content} /> /* Render the Post component for each post */
+          )}
         {error && (
           <h1>{error}</h1>
         )}
       </div>
     </>
-  )
+  );
 }
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import Login from "../pages/Login/Login"
+
 import { useNavigate } from "react-router-dom"
 
 export default function PrivateRoute({ user, children }) {
