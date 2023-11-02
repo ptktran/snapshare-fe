54bcea0e9aa843f416d9c8af9cde79da5061ed19
Da Min Joo <damin.joo0725@gmail.com>
2023-11-01 21:09:38 -0400

    Update Search.jsx

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -63,7 +63,9 @@ export default function Search() {
                         <ul className="list">
                             {users.filter((user) =>
                                 user.username.toLowerCase().includes(query)).map((user) => (
-                                <li key={user.id} className="listItem">{user.username}</li>
+                                <li key={user.id} className="listItem">
+                                    <a href="#">{user.username}</a>
+                                </li>
                             ))}
                         </ul>
                     </div>
@@ -78,7 +80,64 @@ export default function Search() {
                         <ul className="list">
                             {posts.filter((post) =>
                                 post.caption.toLowerCase().includes(query2)).map((post) => (
-                                <li key={post.post_id} className="listItem">{post.caption}</li>
+                                <div>
+                                    <li key={post.post_id} className="listItem">
+                                        <a href="#">{post.caption}</a>
+                                    </li>
+                                {/*  d  
+                                    {/* Popup image        
+                                    <div class="popup" id="imagePop">
+                                        <button onClick={()=>{closeImg()}}>&times;</button>
+
+                                        <div class="flex">
+                                            <div>
+                                                {profileImage ? (
+                                                    <img src={profileImage} class="profile_image" />
+                                                    ) : (
+                                                    <div className="profile-image-placeholder">
+                                                    <img src="https://cdn141.picsart.com/357697367045201.jpg" class="profile_image" />
+                                                    </div>
+                                                )}
+                                            </div>
+                                            <div className="block">
+                                                <h3>{username}</h3>
+                                                <p class="right">{post3.created_at}</p>
+                                            </div>
+                                        </div>
+                                    
+                                        {/* Image Carousel    
+                                        <div class="center"> 
+                                            <div class="carousel_container">
+                                                {/* Image Carousel in edit
+                                                {getSlides(post.file_url)}
+                                            
+                                                <div class="carousel" style={{ transform: `translateX(-${curr * 100}%)` }}>
+                                                {Array.isArray(slides) ? (
+                                                    slides.map((slide) => (
+                                                    isImageLink(slide) ? (
+                                                        <img src={slide} alt="Image" class="carousel_content"/>
+                                                    ) : isVideoLink(slide) ? (
+                                                        <video src={slide} controls loop class="carousel_content" />
+                                                    ) : (
+                                                        <h1 class="carousel_content">No media</h1>
+                                                    )
+                                                    ))
+                                                ) : (
+                                                    <h1>No slides to display</h1>
+                                                )}
+                                                </div>
+                                            </div> 
+                                        </div>  
+                                        <div width="600px">
+                                            <button onClick={prev}>Prev</button>  
+                                            <button class="right" onClick={next}>Next</button>                                          
+                                            <h3 class="caption">{post.caption}</h3>
+                                            <button class="right" onClick={()=>{deletePost(post3.post_id)}}>Delete Post</button>
+                                            <br/>
+                                        </div>
+                                    </div>
+                                */}
+                                </div>   
                             ))}
                         </ul>
                     </div>
